8493653c1cff2a1609340c618b1825d25d73fbf7
Maksym <66667990+HonestProger@users.noreply.github.com>
2022-12-30 08:56:37 +0200

    Add new section and new animation
    Added a new section called "main-nike". Added new animations for button, image content and sneaker menu.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function buttonHover(){ 
+    document.querySelector('.main__button').style.background="linear-gradient(296deg, #179B93 43.2%, rgba(217, 217, 217, 0.71) 61.6%)";
+
+}
+
+function buttonUnHover(){ 
+    document.querySelector('.main__button').style.background="linear-gradient(106.81deg, #179B93 43.2%, rgba(217, 217, 217, 0.71) 61.6%)";
+
+}
+
+
